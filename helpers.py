ad07131d53a4c156aa2fad90171d8250be057201
Ashish Anil Sikaria <88903018+ashishanilsikaria@users.noreply.github.com>
2025-09-17 20:42:08 +0000

    Add placeholder function for PPTX report creation in helpers

--- helpers.py
+++ helpers.py
@@ -66,6 +66,12 @@ def list_to_html_ol(cell):
     return cell
 
 
+def create_pptx_report(findings):
+    # To do
+    # Take the findings and create a pptx report
+    return None
+
+
 def setup_logger(name, log_file, level=logging.INFO):
     """
     Sets up a logger that writes to a specified file and the console.
