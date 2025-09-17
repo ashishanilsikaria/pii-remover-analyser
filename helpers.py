3151e1edbd4a2f054dc874fe1fa477dfddfb4f89
Ashish Anil Sikaria <88903018+ashishanilsikaria@users.noreply.github.com>
2025-09-17 20:34:35 +0000

    Implement placeholder for PDF content extraction in extract_content_from_pdf function

--- helpers.py
+++ helpers.py
@@ -53,10 +53,10 @@ def extract_content_from_pptx(file):
 
 def extract_content_from_pdf(file):
 
-    reader = PdfReader(file)
+    # To do
+    # Extract text and images from PDF
     text = ""
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
+
     return text
 
 
